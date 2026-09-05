cfd5b10132dd88609dd386bd0b82c43d5370fdb1
PauloRugani <phrugany12020511@gmail.com>
2026-09-05 17:46:36 -0300

    chore: change print lang

--- src/utils/push_github.py
+++ src/utils/push_github.py
@@ -10,15 +10,15 @@ def push_to_github(
 ) -> None:
     auth_token = token or os.getenv("GITHUB_TOKEN")
     if not auth_token:
-        raise ValueError("Token do GitHub não fornecido nem encontrado na variável GITHUB_TOKEN.")
+        raise ValueError("GITHUB TOKEN NOT FOUND.")
 
-    g = Github(auth_token)
-    repo = g.get_repo(repo_name)
+    github = Github(auth_token)
+    repo = github.get_repo(repo_name)
 
     with open(local_file_path, "rb") as f:
         content = f.read()
 
-    commit_message = f"chore(data): atualiza {os.path.basename(repo_file_path)} [skip ci]"
+    commit_message = f"[PIPELINE] Updated {os.path.basename(repo_file_path)} [skip ci]"
 
     try:
         remote_file = repo.get_contents(repo_file_path, ref=branch)
@@ -29,15 +29,15 @@ def push_to_github(
             sha=remote_file.sha,
             branch=branch
         )
-        print(f"[GitHub] Atualizado: {repo_file_path}")
+        print(f"[PIPELINE] Updated: {repo_file_path}")
     except GithubException as e:
         if e.status == 404:
             repo.create_file(
                 path=repo_file_path,
-                message=f"chore(data): adiciona {os.path.basename(repo_file_path)} [skip ci]",
+                message=f"[PIPELINE] Added {os.path.basename(repo_file_path)} [skip ci]",
                 content=content,
                 branch=branch
             )
-            print(f"[GitHub] Criado: {repo_file_path}")
+            print(f"[PIPELINE] Created: {repo_file_path}")
         else:
             raise e
