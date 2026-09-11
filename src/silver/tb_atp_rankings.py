ab118cf3e523487a6c28bda9fd0e8b5135901cbb
PauloRugani <phrugany12020511@gmail.com>
2026-09-11 00:59:17 -0300

    feat: update silver to airflow

--- src/silver/tb_atp_rankings.py
+++ src/silver/tb_atp_rankings.py
@@ -1,87 +1,113 @@
+import os
+import pandas as pd
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
 from pyspark.sql.window import Window
-import pandas as pd
+from dotenv import load_dotenv
 
-import os
+load_dotenv()
 os.environ['SPARK_LOCAL_IP'] = '127.0.0.1'
 
-from dotenv import load_dotenv
-load_dotenv()
+def create_spark_session():
+    try:
+        print("Creating spark session...")
+        spark = (
+            SparkSession.builder.appName("silver_atp_ranking")
+            .config("spark.driver.memory", "3500m")
+            .config("spark.executor.memory", "3500m")
+            .config("spark.sql.shuffle.partitions", "8")
+            .config("spark.default.parallelism", "8")
+            .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3")
+            .config("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", "2")
+            .getOrCreate()
+        )
+        
+        spark.conf.set("spark.sql.repl.eagerEval.enabled", True)
+        spark.conf.set("spark.sql.repl.eagerEval.maxNumRows", 200)
+        spark.conf.set("spark.sql.repl.eagerEval.truncate", 50)
+        return spark
+    except Exception as e:
+        print(e)
+        raise
 
+def load_tables(spark):
+    try:
+        print("Loading data...")
+        tb_atp_rankings = (
+            spark.read
+            .format("jdbc")
+            .option("url", os.getenv("JDBC_URL"))
+            .option("dbtable", "bronze.tb_atp_rankings")
+            .option("user", os.getenv("DB_USER"))
+            .option("password", os.getenv("DB_PASSWORD"))
+            .option("driver", "org.postgresql.Driver")
+            .load()
+        )
+        print("Data loaded")
+        return tb_atp_rankings
+    except Exception as e:
+        print(e)
+        raise
 
-try:
-    spark = (
-        SparkSession.builder.appName("silver_atp_ranking")
-        .config("spark.driver.memory", "3500m")
-        .config("spark.executor.memory", "3500m")
-        .config("spark.sql.shuffle.partitions", "8")
-        .config("spark.default.parallelism", "8")
-        .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3")
-        .config("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", "2")
-        .getOrCreate()
-    )
-except Exception as e:
-    print(e)
+def run_transformation(spark, tb_atp_rankings):
+    try:
+        print("Running transformations...")
+        df = (
+            tb_atp_rankings
+            .select(
+                f.date_format(f.to_date(f.col("date"), 'yyyyMMdd'), 'yyyy-MM-dd').alias("DATE_WEEK_RANKING"),
+                f.col("rank").cast("int").alias("NUM_PLAYER_RANK"),
+                f.col("name").alias("DES_PLAYER_NAME"),
+                f.col("id").alias("COD_PLAYER_ID"),
+                f.col("age").cast("int").alias("NUM_PLAYER_AGE"),
+                f.regexp_replace(f.col("points"), ",", "").cast("int").alias("NUM_PLAYER_RANK_PTS"),
+                f.col("lost_earned_points").cast("string").alias("NUM_PLAYER_LE_PTS"),
+                f.col("tourn_played").cast("int").alias("NUM_PLAYER_TOURNEY_PLAYED"),
+                f.regexp_replace(f.col("dropping"), ",", "").cast("int").alias("NUM_PLAYER_DROP_PTS"),
+                f.col("next_best").cast("int").alias("NUM_PLAYER_NEXT_BEST"),
 
+                f.col("DATE_INGESTION").alias("DATE_INGESTION")
+            )
+            .dropDuplicates(["DATE_WEEK_RANKING", "COD_PLAYER_ID"])
+        )
+        print("Transformations completed")
+        return df
+    except Exception as e:
+        print(e)
+        raise
 
-spark.conf.set("spark.sql.repl.eagerEval.enabled", True)
+def save_table(df):
+    try:
+        print("Saving data...")
+        (
+            df.write
+            .format("jdbc")
+            .option("url", os.getenv("JDBC_URL"))
+            .option("dbtable", "silver.tb_atp_rankings")
+            .option("user", os.getenv("DB_USER"))
+            .option("password", os.getenv("DB_PASSWORD"))
+            .option("driver", "org.postgresql.Driver")
+            .mode("overwrite")
+            .save()
+        )
+        print("Data saved to database")
+    except Exception as e:
+        print(e)
+        raise
 
-spark.conf.set("spark.sql.repl.eagerEval.maxNumRows", 200)
-spark.conf.set("spark.sql.repl.eagerEval.truncate", 50)
+def run():
+    spark = None
+    try:
+        spark = create_spark_session()
+        tb_atp_rankings = load_tables(spark)
+        df_final = run_transformation(spark, tb_atp_rankings)
+        save_table(df_final)
+    finally:
+        if spark:
+            print("Stopping spark session...")
+            spark.stop()
+            print("Spark session stopped.")
 
-
-tb_atp_rankings = (
-    spark.read
-    .format("jdbc")
-    .option("url", os.getenv("JDBC_URL"))
-    .option("dbtable", "bronze.tb_atp_rankings")
-    .option("user", os.getenv("DB_USER"))
-    .option("password", os.getenv("DB_PASSWORD"))
-    .option("driver", "org.postgresql.Driver")
-    .load()
-    )
-
-# tb_atp_rankings = spark.read.format("parquet").load(r"data/bronze/tb_atp_rankings/")
-
-
-df = (
-    tb_atp_rankings
-    .select(
-        f.date_format(f.to_date(f.col("date"), 'yyyyMMdd'), 'yyyy-MM-dd').alias("DATE_WEEK_RANKING"),
-        f.col("rank").cast("int").alias("NUM_PLAYER_RANK"),
-        f.col("name").alias("DES_PLAYER_NAME"),
-        f.col("id").alias("COD_PLAYER_ID"),
-        f.col("age").cast("int").alias("NUM_PLAYER_AGE"),
-        f.regexp_replace(f.col("points"), ",", "").cast("int").alias("NUM_PLAYER_RANK_PTS"),
-        f.col("lost_earned_points").cast("string").alias("NUM_PLAYER_LE_PTS"),
-        f.col("tourn_played").cast("int").alias("NUM_PLAYER_TOURNEY_PLAYED"),
-        f.regexp_replace(f.col("dropping"), ",", "").cast("int").alias("NUM_PLAYER_DROP_PTS"),
-        f.col("next_best").cast("int").alias("NUM_PLAYER_NEXT_BEST"),
-
-        f.col("DATE_INGESTION").alias("DATE_INGESTION")
-    )
-    .dropDuplicates(["DATE_WEEK_RANKING", "COD_PLAYER_ID"])
-)
-
-
-(
-    df.coalesce(4).write
-    .mode("overwrite")
-    .option("compression", "snappy")
-    .parquet(r"data/silver/tb_atp_rankings")
-)
-
-
-(
-df.write
-    .format("jdbc")
-    .option("url", os.getenv("JDBC_URL"))
-    .option("dbtable", "silver.tb_atp_rankings")
-    .option("user", os.getenv("DB_USER"))
-    .option("password", os.getenv("DB_PASSWORD"))
-    .option("driver", "org.postgresql.Driver")
-    .mode("overwrite")
-    .save()
-)
+if __name__ == "__main__":
+    run()
 
--- src/silver/tb_atp_tournaments.py
+++ src/silver/tb_atp_tournaments.py
@@ -1,104 +1,128 @@
+import os
+import pandas as pd
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
 from pyspark.sql.window import Window
+from dotenv import load_dotenv
 
-import pandas as pd
-
-import os
+load_dotenv()
 os.environ['SPARK_LOCAL_IP'] = '127.0.0.1'
 
-from dotenv import load_dotenv
-load_dotenv()
+def create_spark_session():
+    try:
+        print("Creating spark session...")
+        spark = (
+            SparkSession.builder.appName("silver_atp_tournament")
+            .config("spark.driver.memory", "4g")
+            .config("spark.executor.memory", "4g")
+            .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3")
+            .config("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", "2")
+            .getOrCreate()
+        )
+        
+        spark.conf.set("spark.sql.repl.eagerEval.enabled", True)
+        spark.conf.set("spark.sql.repl.eagerEval.maxNumRows", 200)
+        spark.conf.set("spark.sql.repl.eagerEval.truncate", 50)
+        return spark
+    except Exception as e:
+        print(e)
+        raise
 
+def load_tables(spark):
+    try:
+        print("Loading data...")
+        tb_atp_matches = (
+            spark.read
+            .format("jdbc")
+            .option("url", os.getenv("JDBC_URL"))
+            .option("dbtable", "bronze.tb_atp_matches")
+            .option("user", os.getenv("DB_USER"))
+            .option("password", os.getenv("DB_PASSWORD"))
+            .option("driver", "org.postgresql.Driver")
+            .load()
+        )
+        print("Data loaded")
+        return tb_atp_matches
+    except Exception as e:
+        print(e)
+        raise
 
-try:
-    spark = (
-        SparkSession.builder.appName("silver_atp_tournament")
-        .config("spark.driver.memory", "4g")
-        .config("spark.executor.memory", "4g")
-        .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3")
-        .config("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", "2")
-        .getOrCreate()
-    )
-except Exception as e:
-    print(e)
+def run_transformation(spark, tb_atp_matches):
+    try:
+        print("Running transformations...")
+        df = (
+            tb_atp_matches
+            .withColumn(
+                "TOURNEY_NAME",
+                f.when(f.col("tourney_name").contains("Davis Cup"), f.lit("Davis Cup"))
+                .when(f.col("tourney_id") == "2026-416", f.lit("Rome Masters"))
+                .otherwise(f.col("tourney_name"))
+            )
+            .withColumn(
+                "TOURNEY_LEVEL",
+                f.when(f.col("tourney_id") == "2026-416", f.lit("M"))
+                .when(f.col("tourney_name").contains("Olympics"), f.lit("O"))
+                .when(f.col("tourney_name").contains("Finals"), f.lit("F"))
+                .otherwise(f.col("tourney_level"))
+            )
+            .withColumn(
+                "TOURNEY_IS_INDOOR",
+                f.when(f.col("indoor") == "I", f.lit(True))
+                .when(f.col("surface") == "Carpet", f.lit(True))
+                .when(f.col("indoor") == "O", f.lit(False))
+                .when(f.col("tourney_name").contains("Indoor"), f.lit(True))
+                .otherwise(f.lit(False))
+            )
+            .groupBy(f.col("tourney_id").alias("COD_TOURNEY_ID"))
+            .agg(
+                f.first("TOURNEY_NAME", ignorenulls=True).alias("DES_TOURNEY_NAME"),
+                f.first("TOURNEY_LEVEL", ignorenulls=True).alias("DES_TOURNEY_LEVEL"),
+                f.first(f.col("draw_size").cast("int"), ignorenulls=True).cast("int").alias("NUM_TOURNEY_DRAW_SIZE"),
+                f.first("surface", ignorenulls=True).alias("DES_TOURNEY_SURFACE"),
+                f.first("TOURNEY_IS_INDOOR", ignorenulls=True).alias("FLAG_TOURNEY_IS_INDOOR"),
+                f.substring(f.first("tourney_date", ignorenulls=True).cast("string"), 1, 4).alias("REF_YEAR"),
+                f.first("DATE_INGESTION").alias("DATE_INGESTION")
+            )
+            .dropDuplicates(["COD_TOURNEY_ID"])
+        )
+        print("Transformations completed")
+        return df
+    except Exception as e:
+        print(e)
+        raise
 
+def save_table(df):
+    try:
+        print("Saving data...")
+        (
+            df.write
+            .format("jdbc")
+            .option("url", os.getenv("JDBC_URL"))
+            .option("dbtable", "silver.tb_atp_tournaments")
+            .option("user", os.getenv("DB_USER"))
+            .option("password", os.getenv("DB_PASSWORD"))
+            .option("driver", "org.postgresql.Driver")
+            .mode("overwrite")
+            .save()
+        )
+        print("Data saved to database")
+    except Exception as e:
+        print(e)
+        raise
 
-spark.conf.set("spark.sql.repl.eagerEval.enabled", True)
+def run():
+    spark = None
+    try:
+        spark = create_spark_session()
+        tb_atp_matches = load_tables(spark)
+        df_final = run_transformation(spark, tb_atp_matches)
+        save_table(df_final)
+    finally:
+        if spark:
+            print("Stopping spark session...")
+            spark.stop()
+            print("Spark session stopped.")
 
-spark.conf.set("spark.sql.repl.eagerEval.maxNumRows", 200)
-spark.conf.set("spark.sql.repl.eagerEval.truncate", 50)
-
-
-tb_atp_matches = (
-    spark.read
-    .format("jdbc")
-    .option("url", os.getenv("JDBC_URL"))
-    .option("dbtable", "bronze.tb_atp_matches")
-    .option("user", os.getenv("DB_USER"))
-    .option("password", os.getenv("DB_PASSWORD"))
-    .option("driver", "org.postgresql.Driver")
-    .load()
-    )
-
-
-# tb_atp_matches = spark.read.format("parquet").load(r"data/bronze/tb_atp_matches/")
-
-
-df = (
-    tb_atp_matches
-    .withColumn(
-        "TOURNEY_NAME",
-        f.when(f.col("tourney_name").contains("Davis Cup"), f.lit("Davis Cup"))
-         .when(f.col("tourney_id") == "2026-416", f.lit("Rome Masters"))
-         .otherwise(f.col("tourney_name"))
-    )
-    .withColumn(
-        "TOURNEY_LEVEL",
-        f.when(f.col("tourney_id") == "2026-416", f.lit("M"))
-         .when(f.col("tourney_name").contains("Olympics"), f.lit("O"))
-         .when(f.col("tourney_name").contains("Finals"), f.lit("F"))
-         .otherwise(f.col("tourney_level"))
-    )
-    .withColumn(
-        "TOURNEY_IS_INDOOR",
-        f.when(f.col("indoor") == "I", f.lit(True))
-         .when(f.col("surface") == "Carpet", f.lit(True))
-         .when(f.col("indoor") == "O", f.lit(False))
-         .when(f.col("tourney_name").contains("Indoor"), f.lit(True))
-         .otherwise(f.lit(False))
-    )
-    .groupBy(f.col("tourney_id").alias("COD_TOURNEY_ID"))
-    .agg(
-        f.first("TOURNEY_NAME", ignorenulls=True).alias("DES_TOURNEY_NAME"),
-        f.first("TOURNEY_LEVEL", ignorenulls=True).alias("DES_TOURNEY_LEVEL"),
-        f.first(f.col("draw_size").cast("int"), ignorenulls=True).cast("int").alias("NUM_TOURNEY_DRAW_SIZE"),
-        f.first("surface", ignorenulls=True).alias("DES_TOURNEY_SURFACE"),
-        f.first("TOURNEY_IS_INDOOR", ignorenulls=True).alias("FLAG_TOURNEY_IS_INDOOR"),
-        f.substring(f.first("tourney_date", ignorenulls=True).cast("string"), 1, 4).alias("REF_YEAR"),
-        f.first("DATE_INGESTION").alias("DATE_INGESTION")
-    )
-    .dropDuplicates(["COD_TOURNEY_ID"])
-)
-
-
-(
-    df.write
-    .mode("overwrite")
-    .option("compression", "snappy")
-    .parquet(r"../../data/silver/tb_atp_tournaments")
-)
-
-
-(
-df.write
-    .format("jdbc")
-    .option("url", os.getenv("JDBC_URL"))
-    .option("dbtable", "silver.tb_atp_tournaments")
-    .option("user", os.getenv("DB_USER"))
-    .option("password", os.getenv("DB_PASSWORD"))
-    .option("driver", "org.postgresql.Driver")
-    .mode("overwrite")
-    .save()
-)
+if __name__ == "__main__":
+    run()
 
